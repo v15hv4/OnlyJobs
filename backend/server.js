2b37663ece0feba9ae4c6f09fdf8291dee2e4566
v15hv4 <vishva2912@gmail.com>
2021-01-11 11:32:54 +0530

    Add mongoose integrations

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
+const port = process.env.PORT || 5000;
+const mongo_uri = process.env.MONGO_URI;
+
+app.use(express.json());
+
+mongoose.connect(mongo_uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => res.send("helo"));
 
-app.listen(process.env.PORT || 5000, () => console.log("Running..."));
+app.listen(port, () => console.log(`Started server on port ${port}.`));
+mongoose.connection.once("open", () => console.log("Connected to MongoDB."));
