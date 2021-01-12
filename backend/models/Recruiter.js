0826638435d03ed2c04aac863b213849a238206f
v15hv4 <vishva2912@gmail.com>
2021-01-12 17:31:54 +0530

    Add validators for Recruiter

--- backend/models/Recruiter.js
+++ backend/models/Recruiter.js
@@ -1,17 +1,22 @@
 import { Schema } from "mongoose";
+import { phone_number, word_limit } from "./validators";
 import User from "./User";
 
 const RecruiterSchema = new Schema({
     contact: {
         type: String,
         required: true,
+        validate: {
+            validator: (v) => phone_number(v),
+            message: "Invalid phone number!",
+        },
     },
     bio: {
         type: String,
         required: true,
         validate: {
-            validator: (v) => v.split(" ").length <= 250,
-            message: "Bio can not exceed 250 words!",
+            validator: (v) => word_limit(v, 250),
+            message: "Bio may not exceed 250 words!",
         },
     },
 });
--- backend/models/validators.js
+++ backend/models/validators.js
@@ -11,3 +11,8 @@ export function future_date(past, future) {
 export function word_limit(text, limit) {
     return text.split(" ").length <= limit;
 }
+
+export function phone_number(number) {
+    const len = number.replace(/[^\d]/g, "").length;
+    return len > 7 && len < 13;
+}
