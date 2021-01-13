20276782c34d995ed2f58bfc283a79dd3d3be0e5
v15hv4 <vishva2912@gmail.com>
2021-01-14 00:39:05 +0530

    Make populate script async and reflect new changes

--- backend/populate.py
+++ backend/populate.py
@@ -1,6 +1,7 @@
+from requests_async import Session
+from asyncio import get_event_loop
 from random import randint, choice
 from json import loads, dumps
-from requests import Session
 from faker import Faker
 from sys import exit
 
@@ -12,104 +13,124 @@ f = Faker()
 
 # control
 targets = {
-    "jobs": {"endpoint": "/api/jobs/new", "n": 0},
-    "languages": {"endpoint": "/api/languages/new", "n": 10},
-    "recruiters": {"endpoint": "/api/recruiters/new", "n": 10},
-    "applicants": {"endpoint": "/api/applicants/new", "n": 10},
-    "applications": {"endpoint": "/api/applications/new", "n": 0},
+    "jobs": {"endpoint": "/api/jobs/new", "n": 20},
+    "languages": {"endpoint": "/api/languages/new", "n": 30},
+    "recruiters": {"endpoint": "/api/auth/register", "n": 10},
+    "applicants": {"endpoint": "/api/auth/register", "n": 40},
+    "applications": {"endpoint": "/api/applications/new", "n": 50},
 }
 
 
-def populate(endpoint, data):
-    r = s.post(f"{host}{endpoint}", data=data, headers=headers)
+async def populate(endpoint, data):
+    r = await s.post(f"{host}{endpoint}", data=data, headers=headers)
     if r.status_code != 200:
         exit(f"FAILED:\n\nendpoint: {endpoint}\n\ndata: {data}\n\nresponse: {r.content}")
 
 
-print("POPULATING...")
+async def main():
+    print("POPULATING...")
 
-# populate languages
-n = targets["languages"]["n"]
-endpoint = targets["languages"]["endpoint"]
-for _ in range(n):
-    data = dumps({"name": f.name().split(" ")[0]})
-    populate(endpoint, data)
+    # populate languages {{{
+    print("> languages... ", end="")
+    n = targets["languages"]["n"]
+    endpoint = targets["languages"]["endpoint"]
+    for _ in range(n):
+        data = dumps({"name": f.name().split(" ")[0]})
+        await populate(endpoint, data)
+    # }}}
 
-# populate recruiters
-n = targets["recruiters"]["n"]
-endpoint = targets["recruiters"]["endpoint"]
-for _ in range(n):
-    data = dumps(
-        {
-            "email": f.email(),
-            "password": f.pystr(),
-            "name": f.name(),
-            "contact": f.phone_number(),
-            "bio": f.text(),
-        }
-    )
-    populate(endpoint, data)
+    # populate recruiters {{{
+    n = targets["recruiters"]["n"]
+    endpoint = targets["recruiters"]["endpoint"]
+    for _ in range(n):
+        data = dumps(
+            {
+                "email": f.email(),
+                "password": f.pystr(),
+                "name": f.name(),
+                "contact": f.phone_number(),
+                "bio": f.text(),
+                "role": "recruiter",
+            }
+        )
+        await populate(endpoint, data)
+    print("done!")
+    # }}}
 
-# populate applicants
-n = targets["applicants"]["n"]
-endpoint = targets["applicants"]["endpoint"]
-languages = loads(s.get(f"{host}/api/languages").text)
-for _ in range(n):
-    data = dumps(
-        {
-            "email": f.email(),
-            "password": f.pystr(),
-            "name": f.name(),
-            "education": [
-                {
-                    "name": f.company(),
-                    "start_year": f.year(),
-                    "end_year": f.year() if randint(0, 1) == 0 else None,
-                }
-                for i in range(randint(0, 3))
-            ],
-            "skills": list(set([choice(languages)["_id"] for i in range(randint(0, 5))])),
-        }
-    )
-    populate(endpoint, data)
+    # populate applicants {{{
+    print("> applicants... ", end="")
+    n = targets["applicants"]["n"]
+    endpoint = targets["applicants"]["endpoint"]
+    languages = loads((await s.get(f"{host}/api/languages")).text)
+    for _ in range(n):
+        data = dumps(
+            {
+                "email": f.email(),
+                "password": f.pystr(),
+                "name": f.name(),
+                "education": [
+                    {
+                        "name": f.company(),
+                        "start_year": f.year(),
+                        "end_year": f.year() if randint(0, 1) == 0 else None,
+                    }
+                    for i in range(randint(0, 3))
+                ],
+                "skills": list(set([choice(languages)["_id"] for i in range(randint(0, 5))])),
+                "role": "applicant",
+            }
+        )
+        await populate(endpoint, data)
+    print("done!")
+    # }}}
 
-# populate jobs
-n = targets["jobs"]["n"]
-endpoint = targets["jobs"]["endpoint"]
-languages = loads(s.get(f"{host}/api/languages").text)
-recruiters = loads(s.get(f"{host}/api/recruiters").text)
-for _ in range(n):
-    data = dumps(
-        {
-            "title": f.job(),
-            "recruiter": choice(recruiters),
-            "max_applications": randint(1, 20),
-            "max_positions": randint(1, 10),
-            "deadline": f.date_this_year(before_today=False, after_today=True).isoformat(),
-            "skillset": list(set([choice(languages)["_id"] for i in range(randint(1, 5))])),
-            "type": choice(["full_time", "part_time", "work_from_home"]),
-            "duration": randint(0, 6),
-            "salary": randint(10000, 80000),
-            "rating": {"value": randint(0, 5), "amount": randint(1, 10),},
-        }
-    )
-    populate(endpoint, data)
+    # populate jobs {{{
+    print("> jobs... ", end="")
+    n = targets["jobs"]["n"]
+    endpoint = targets["jobs"]["endpoint"]
+    languages = loads((await s.get(f"{host}/api/languages")).text)
+    recruiters = loads((await s.get(f"{host}/api/recruiters")).text)
+    for _ in range(n):
+        data = dumps(
+            {
+                "title": f.job(),
+                "recruiter": choice(recruiters),
+                "max_applications": randint(1, 20),
+                "max_positions": randint(1, 10),
+                "deadline": f.date_this_year(before_today=False, after_today=True).isoformat(),
+                "skillset": list(set([choice(languages)["_id"] for i in range(randint(1, 5))])),
+                "type": choice(["full_time", "part_time", "work_from_home"]),
+                "duration": randint(0, 6),
+                "salary": randint(10000, 80000),
+            }
+        )
+        await populate(endpoint, data)
+    print("done!")
+    # }}}
 
-# populate applications
-n = targets["applications"]["n"]
-endpoint = targets["applications"]["endpoint"]
-applicants = loads(s.get(f"{host}/api/applicants").text)
-jobs = loads(s.get(f"{host}/api/jobs").text)
-for _ in range(n):
-    data = dumps(
-        {
-            "applicant": applicants[0],
-            "job": jobs[0],
-            "SOP": f.text(),
-            "state": choice(["applied", "shortlisted", "accepted", "rejected", "deleted"]),
-        }
-    )
-    populate(endpoint, data)
+    # populate applications {{{
+    print("> applications... ", end="")
+    n = targets["applications"]["n"]
+    endpoint = targets["applications"]["endpoint"]
+    applicants = loads((await s.get(f"{host}/api/applicants")).text)
+    jobs = loads((await s.get(f"{host}/api/jobs")).text)
+    for _ in range(n):
+        data = dumps(
+            {
+                "applicant": applicants[0],
+                "job": jobs[0],
+                "SOP": f.text(),
+                "state": choice(["applied", "shortlisted", "accepted", "rejected", "deleted"]),
+            }
+        )
+        await populate(endpoint, data)
+    print("done!")
+    # }}}
+
+    exit("ALL DONE.")
 
 
-exit("DONE.")
+if __name__ == "__main__":
+    loop = get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
