153b75f646e3bb763ad5a8f20dd0a761e183104e
v15hv4 <vishva2912@gmail.com>
2021-01-15 14:32:41 +0530

    Introduce logos and login page skeleton

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -4,9 +4,19 @@ import PageContainer from "components/PageContainer";
 const Login = () => {
     return (
         <PageContainer>
-            <Row className="h-75">
-                <Col className="d-flex justify-content-center align-items-center"> logo </Col>
-                <Col className="d-flex justify-content-center align-items-center"> form </Col>
+            <Row className="h-100">
+                <Col
+                    sm={7}
+                    className="d-flex flex-column justify-content-center align-items-center"
+                >
+                    <img src="/onlyjobs_full.svg" alt="OnlyJobs" className="w-75" />
+                    <div className="mt-4 h4 font-italic font-weight-light">
+                        &lt;insert epic tagline here&gt;
+                    </div>
+                </Col>
+                <Col className="d-flex justify-content-center align-items-center bg-dark text-light">
+                    form
+                </Col>
             </Row>
         </PageContainer>
     );
