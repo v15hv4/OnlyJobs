ecd3edf4c2a0850716d4228a619c5cf333d94bec
v15hv4 <vishva2912@gmail.com>
2021-01-23 03:24:57 +0530

    Add rating stars to JobItem

--- frontend/src/components/JobsList/JobItem.jsx
+++ frontend/src/components/JobsList/JobItem.jsx
@@ -1,4 +1,5 @@
-import { Card, CardBody, CardHeader, CardFooter, Button, Alert } from "reactstrap";
+import { Card, CardBody, CardHeader, CardFooter, Button } from "reactstrap";
+import StarRatingComponent from "react-star-rating-component";
 
 const JobItem = ({
     deadline,
@@ -26,10 +27,50 @@ const JobItem = ({
                 <div>{deadline} to apply</div>
                 <div className="d-flex justify-content-between">
                     <div>₹{new Intl.NumberFormat("en-IN").format(salary)}</div>
-                    <div>{`${rating.value} (${rating.amount})`}</div>
+                    <div className="d-flex">
+                        <div className="h5">
+                            <StarRatingComponent
+                                editing={false}
+                                starColor="#ffb400"
+                                emptyStarColor="#ffb400"
+                                value={rating.value}
+                                starCount={5}
+                                renderStarIcon={(index, value) => {
+                                    return (
+                                        <span>
+                                            <i
+                                                className={
+                                                    index <= value ? "fas fa-star" : "far fa-star"
+                                                }
+                                            />
+                                        </span>
+                                    );
+                                }}
+                                renderStarIconHalf={() => {
+                                    return (
+                                        <span>
+                                            <span style={{ position: "absolute" }}>
+                                                <i className="far fa-star" />
+                                            </span>
+                                            <span>
+                                                <i className="fas fa-star-half" />
+                                            </span>
+                                        </span>
+                                    );
+                                }}
+                            />
+                        </div>
+                        <div className="ml-1 text-muted">({rating.amount})</div>
+                    </div>
                 </div>
             </CardBody>
-            <CardFooter>{state}</CardFooter>
+            <CardFooter className="pt-0">
+                {state === "available" && (
+                    <Button type="button" color="primary" className="fw-700 w-100 text-uppercase">
+                        Apply
+                    </Button>
+                )}
+            </CardFooter>
         </Card>
     );
 };
